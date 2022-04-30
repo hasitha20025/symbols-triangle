c32ab3e1478e8affec1677b32f5822a2c52f7c4d
Hasitha Dhananjaya <96529549+hasitha20025@users.noreply.github.com>
2022-04-30 08:10:11 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    int i,j,row ;
+    char symbol;
+    printf("Input symbol:-");
+    scanf("%c",&symbol);
+    printf("Enter the number of rows:-");
+    scanf("%d",&row);
+
+    for(i=1;i<=row;i++){
+        for(j=1;j<=i;j++){
+            printf("%c",symbol);
+        }
+        printf("\n");
+    }
+    return 0;
+}
